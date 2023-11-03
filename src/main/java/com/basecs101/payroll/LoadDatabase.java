4e580445be37c2a05805297182892040daafa817
Vikram Gupta <basecs101@gmail.com>
2023-11-03 22:31:34 +0530

    Initial project with Employee.java and it's EmployeeRepository and config class to load few entries in h2 in memory db.

---
+++ src/main/java/com/basecs101/payroll/LoadDatabase.java
@@ -0,0 +1,20 @@
+package com.basecs101.payroll;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class LoadDatabase {
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Komal", "HR")));
+            log.info("Preloading " + repository.save(new Employee("Bhagyashree", "Admin")));
+        };
+    }
+}
